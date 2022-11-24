f9d4b9de0340626af09b18d34dcd9895c29696b9
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2022-11-24 10:45:16 -0500

    asyc-generator

---
+++ src/generator/index.js
@@ -0,0 +1,25 @@
+//Ejemplo 1
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const g = gen();
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+//Ejemplo 2
+
+function* iterate(array) {
+  for (let value of array) {
+    yield value;
+  }
+}
+
+const it = iterate(["Oscar", "Omar", "Lucia", "Juan"]);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
